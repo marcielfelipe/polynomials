9eb4cc08213d92aa5d4dcec4ce313fa7c7ddef3f
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-09 20:56:28 -0300

    linked list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,61 @@ import '../../global.css';
 import './styles.css'
 
 export default function Home(){
+    const list = LinkedList();
+    list.add(1,'ab');
+    list.add(3,'ab');
+    list.print();
+    console.log(list.sum(1));
+
+    function LinkedList(){
+        let head = null;
+        let length = 0;
+    
+        const Node = (coefficient, exponent) => {
+            return {
+                coefficient,
+                exponent,
+                next:null
+            }
+        }
+        const add = (c, e) => {
+            if(!head) {
+                head = Node(c,e)
+                length++
+                return head
+            }
+            let node = head;
+    
+            while(node.next){
+                node = node.next;
+            }
+            node.next = Node(c,e);
+            length++;
+            return node.next
+        }
+        const sum = (index) => {
+            if(length === 0){
+                return null
+            }
+            let node = head;
+            let result = 0;
+            let count = 0;
+            while(count < index && node.next){
+                result += node.coefficient
+                count++
+            }
+            return result
+        }
+    }
+    
+
+    
     return(
-        <h1>Hello World</h1>
+        <ul>
+            {list.map(item=>(
+                <li>{item}</li>
+            ))}
+        </ul>
     );
+    
 }
