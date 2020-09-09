ea117766670315e0cb8cabaf00fc76dc32ab952e
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-09 20:46:54 -0300

    correções

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Home from './pages/Home';
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../global.css';
+import './styles.css'
+
+export default function Home(){
+    return(
+        <h1>Hello World</h1>
+    );
+}
